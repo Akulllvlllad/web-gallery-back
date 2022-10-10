c4beaaa6d1d2860112fa14a62fb8c6710df4b5dc
Vladislav <Akulllvlllad@gmail.com>
2022-10-10 10:51:36 +0700

    10.10.2022

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose
 	.then(() => console.log('BD is OK'))
 	.catch(err => console.log('BD is ERROR', err))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 'https://gallerywebapp.ml'
 
 app.use(cors())
 app.use('/images', express.static('images'))
